7d8bdf3d9e4016c32c1002f9508e797325b2245f
申行 <binary2015nov@outlook.com>
2017-12-06 11:07:27 +0800

    Initialize repository structure

---
+++ src/DocsAppHost.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ServiceStack;
+using ServiceStack.Formats;
+using Docs.Logic;
+
+namespace Docs
+{
+    public class DocsAppHost : AppHostBase
+    {
+        public DocsAppHost() : base("ServiceStack Docs", typeof(PageService).Assembly) { }
+
+        public override void OnBeforeInit()
+        {
+            Config.WebHostUrl = AppSettings.GetString("WebHostUrl");
+            string filePath = Config.WebHostPhysicalPath.CombineWith("Pages.json");
+            PageManager.Default.Init(filePath, Config.WebHostUrl);
+        }
+
+        public override void Configure(Funq.Container container)
+        {
+            Plugins.Add(new MarkdownFormat());
+
+            Routes
+                .Add<Page>("/pages")
+                .Add<Page>("/pages/{Name}")
+                .Add<Category>("/category/{Name}")
+                .Add<Search>("/search")
+                .Add<Search>("/search/{Query}");
+        }
+    }
+}
